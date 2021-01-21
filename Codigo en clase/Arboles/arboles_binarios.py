9344a7a5148929e0da6cd736feedebfa9673a9cd
SebastianZR <sebastianzarraga43@aragon.unam.mx>
2021-01-21 15:41:09 -0600

    Clase del 21 de enero del 2021

--- Codigo en clase/Arboles/arboles_binarios.py
+++ Codigo en clase/Arboles/arboles_binarios.py
@@ -37,13 +37,56 @@ class BinarySearchTree:
             print(nodo.data, end= " , ")
             self.__recorrido_in(nodo.right)
 
+    def __recorrido_pos (self,nodo):
+        if nodo :
+            self.__recorrido_pos(nodo.left)
+            self.__recorrido_pos(nodo.right)
+            print (nodo.data, end = " , ")
+
+    def __recorrido_pre(self, nodo):
+        if nodo :
+            print (nodo.data , end= " , ")
+            self.__recorrido_pre(nodo.left)
+            self.__recorrido_pre(nodo.right)
+
     def transversal (self , format="inorden"):
         if format == "inorden":
             self.__recorrido_in(self.__root)
+            print ("Recorrido in ordenn . \n ")
         elif format == "preorden":
-            print ("Recorrido preoirden")
+            self.__recorrido_pre(self.__root)
+            print ("Recorrido preoirden\n")
         elif format == "posorden":
-            print("Recorrido posorden")
+            self.__recorrido_pos(self.__root)
+            print("Recorrido posorden\n")
         else:
             print ("Error, Formato incompatible ")
         print ()
+
+    def search (self,value):
+        if self.__root == None :
+            return None
+        else:
+            return self.__search (self.__root , value )
+
+    def __search (self , nodo , value):
+
+        if nodo == None :
+            print ("Caso base")
+            return None
+        elif nodo.data == value :
+            print ("Encontrado")
+            return nodo.data
+        elif value < nodo.data :
+            return self.__search(nodo.left , value )
+        else :
+            return self.__search(nodo.right , value )
+
+    def remove (self , value ):
+        encontrado = self.search(value)
+        #caso1
+        if encontrado.left == None and encontrado.right == None :
+            print ("Eliminado ", encontrado.data )
+            encontrado = None
+        elif (encontrado.left != None and encontrado.right == None) or encontrado.left == None and encontrado.right != None :
+            print (f"A eliminar {encontrado.data }" )
